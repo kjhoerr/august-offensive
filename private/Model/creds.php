e46b39ceb2fd359c90536d6b815988398a5513f4
Kevin Hoerr <kjhoerr@protonmail.com>
2017-07-08 11:15:51 -0400

    Update creds.php

--- private/Model/creds.php
+++ private/Model/creds.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 $cred = new \stdClass();
 $cred->host = "localhost";
 $cred->port = "5432";
-$cred->dbName = "august";
-$cred->login = "smallg";
-$cred->password = "abc";
-
+$cred->dbName = "";
+$cred->login = "";
+$cred->password = "";
