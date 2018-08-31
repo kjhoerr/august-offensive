08adaf5464aa7a1903a88d2333f4f53938f74800
Kevin J Hoerr <kjhoerr@protonmail.com>
2018-08-31 19:56:49 -0400

    Format main.rs according to format RFCs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 // specify recurse depth for error_chain
 #![recursion_limit = "1024"]
 
-extern crate dotenv;
-extern crate failure;
 extern crate actix;
 extern crate actix_web;
+extern crate dotenv;
 extern crate env_logger;
+extern crate failure;
 #[macro_use]
 extern crate diesel;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
-pub mod schema;
 pub mod routes;
+pub mod schema;
 
 use actix_web::{middleware, server, App};
-use diesel::prelude::*;
 use diesel::pg::PgConnection;
+use diesel::prelude::*;
 use dotenv::dotenv;
-use std::env;
 use failure::Error;
 use routes::*;
+use std::env;
 
 fn main() {
     if let Err(ref e) = run() {
@@ -33,17 +33,14 @@ fn main() {
 
 fn run() -> Result<(), Error> {
     dotenv().ok();
-    let db_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-    let bind_address = env::var("BIND_ADDRESS")
-        .expect("BIND_ADDRESS must be set");
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let bind_address = env::var("BIND_ADDRESS").expect("BIND_ADDRESS must be set");
 
     env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     let sys = actix::System::new("august-offensive");
 
-    PgConnection::establish(&db_url)
-        .expect(&format!("Error connecting to {}", db_url));
+    PgConnection::establish(&db_url).expect(&format!("Error connecting to {}", db_url));
 
     server::new(|| {
         App::new()
