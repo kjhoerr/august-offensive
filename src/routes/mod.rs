f0cc06b5176c1c429d993573e7e8ba0ef6754714
Kevin J Hoerr <kjhoerr@protonmail.com>
2018-08-31 19:47:09 -0400

    Implement NotUnderstood message; Wrap messages with OutgoingMsg

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,24 +1,37 @@
-use actix_web::{HttpRequest, Json, Result, Responder};
+use actix_web::{HttpRequest, Json, Result};
 use std::ops::Deref;
 
 pub mod messages;
 use messages::*;
 
-pub fn not_understood(_req: &HttpRequest) -> impl Responder {
-    "Hello, world!"
+type JsonMessage<U> = Result<Json<OutgoingMsg<U>>>;
+
+// Sends a default response message when requested an undefined resource.
+pub fn not_understood(req: &HttpRequest) -> JsonMessage<NotUnderstood> {
+    let message = NotUnderstood {
+        path: destruct_path(req.path()),
+    };
+
+    Ok(Json(message.as_outgoing()))
 }
 
-pub fn callback(req: &HttpRequest) -> Result<Json<Callback>> {
+// Sends Callback message with information from HttpRequest.
+pub fn callback(req: &HttpRequest) -> JsonMessage<Callback> {
     let path = req.path();
+    let method = req.method().as_str();
     let query_ref = req.query();
     let request = query_ref.deref().clone();
 
-    Ok(Json(Callback {
+    let callback = Callback {
         path: destruct_path(path),
-        request: request,
-    }))
+        request: String::from(method),
+        content: request,
+    };
+
+    Ok(Json(callback.as_outgoing()))
 }
 
+// Takes an HttpRequest path and splits it into an array.
 fn destruct_path(path: &str) -> Vec<String> {
     path.split_terminator("/")
         // first element is always blank due to link starting with "/api"
