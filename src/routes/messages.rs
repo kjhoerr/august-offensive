4fb3dc1c9d6b74df77a4d56d826b8842481bb377
Kevin J Hoerr <kjhoerr@protonmail.com>
2018-08-31 19:47:09 -0400

    Implement NotUnderstood message; Wrap messages with OutgoingMsg

--- src/routes/messages.rs
+++ src/routes/messages.rs
@@ -1,19 +1,44 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, marker::Sized};
 
-trait Content {}
+#[derive(Serialize)]
+pub struct OutgoingMsg<T> {
+    pub result_type: String,
+    pub content: T,
+}
 
-trait Message {
+pub trait Message {
     fn name(&self) -> String;
+    fn as_outgoing(self) -> OutgoingMsg<Self>
+    where
+        Self: Sized,
+    {
+        OutgoingMsg {
+            result_type: self.name(),
+            content: self,
+        }
+    }
 }
 
 #[derive(Serialize)]
 pub struct Callback {
     pub path: Vec<String>,
-    pub request: HashMap<String, String>,
+    pub request: String,
+    pub content: HashMap<String, String>,
 }
 
 impl Message for Callback {
     fn name(&self) -> String {
         String::from("CALLBACK")
+    }
+}
+
+#[derive(Serialize)]
+pub struct NotUnderstood {
+    pub path: Vec<String>,
+}
+
+impl Message for NotUnderstood {
+    fn name(&self) -> String {
+        String::from("NOT_UNDERSTOOD")
     }
 }
